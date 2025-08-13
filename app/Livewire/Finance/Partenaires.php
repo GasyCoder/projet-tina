5cf36683c4a1fc2d56012f3e3b9d73033418583d
GasyCoder <bezaraflorent@gmail.com>
2025-08-13 14:50:24 +0300

    create composant livewire

---
+++ app/Livewire/Finance/Partenaires.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Livewire\Finance;
+
+use Livewire\Component;
+
+class Partenaires extends Component
+{
+    public function render()
+    {
+        return view('livewire.finance.partenaires');
+    }
+}
